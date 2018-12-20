c7be2ef39310b43c6edfa57b25801e7b851480a7
Devildude4427 <ryanchristian4427@gmail.com>
2018-12-19 23:04:08 -0600

    Base UI exists, starting to work with selenium

---
+++ Selenium/SeleniumScraper.cs
@@ -0,0 +1,15 @@
+﻿using Gtk;
+using Selenium.UserInterface;
+
+namespace Selenium
+{
+    class SeleniumScraper
+    {
+        private static void Main(string[] args)
+        {
+            Application.Init();
+            new MainWindow().Show();
+            Application.Run();
+        }
+    }
+}
