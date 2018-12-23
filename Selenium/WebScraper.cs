6875a2cfaa65fa0f6ada5f43b37ba2b0f762ae6d
Devildude4427 <ryanchristian4427@gmail.com>
2018-12-22 22:07:04 -0600

    Moving to use Chrome, as it's about 14 seconds, or 40% quicker than gecko. Casper is faster yet, which I may yet still move to

--- Selenium/WebScraper.cs
+++ Selenium/WebScraper.cs
@@ -4,6 +4,7 @@ using System.Net;
 using Gtk;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using Pango;
 using Selenium.PageObjects;
 using Selenium.Utility;
@@ -20,13 +21,18 @@ namespace Selenium
 
         public static void SetUp(string targetAccount, bool headless)
         {
-            var options = new FirefoxOptions();
-            if (headless) { options.AddArgument("--headless");}
+            var optionsFireFox = new FirefoxOptions();
+            var optionsChrome = new ChromeOptions();
+            if (headless) { optionsFireFox.AddArgument("--headless"); optionsChrome.AddArgument("headless");}
             
-            options.SetPreference("permissions.default.image", 2);
-            options.SetPreference("dom.ipc.plugins.enabled.libflashplayer.so", false);
+//            optionsFireFox.SetPreference("permissions.default.image", 2);
+//            optionsFireFox.SetPreference("dom.ipc.plugins.enabled.libflashplayer.so", false);
             
-            _driver = new FirefoxDriver(options);
+//            _driver = new FirefoxDriver(options);
+            
+            
+            optionsChrome.AddUserProfilePreference("profile.default_content_setting_values.images", 2);
+            _driver = new ChromeDriver(optionsChrome);
             
             _path = _path + targetAccount + "/";
 
@@ -36,32 +42,41 @@ namespace Selenium
         private static void RunScraper(string targetAccount){
             var resourcesDictionary = new UriNameDictionary();
             var profilePage = new ProfilePage(_driver);
+            var watch = System.Diagnostics.Stopwatch.StartNew();
             
             profilePage.GoToProfile(targetAccount);
             profilePage.GetProfilePicture(resourcesDictionary);
 //            profilePage.EnterStory();
             var postPage = profilePage.EnterPosts();
+            watch.Stop();
+            var enterPostTime = watch.ElapsedMilliseconds;
+            Console.WriteLine("Time to enter post: " + enterPostTime/1000.00 + " seconds");
             
-            var watch = System.Diagnostics.Stopwatch.StartNew();
+            watch.Restart();
             postPage.GetPostData(resourcesDictionary);
             watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
+            var getPostPicturesTime = watch.ElapsedMilliseconds;
+            Console.WriteLine("Time to get all post pictures: " + getPostPicturesTime/1000.00 + " seconds");
             
-            Console.WriteLine("Time to get all post pictures: " + elapsedMs/1000.00 + " seconds");
-            
+            watch.Restart();
             foreach (var entry in resourcesDictionary)
             {
-                Directory.CreateDirectory(_path);
+                if(!File.Exists(_path)) {Directory.CreateDirectory(_path);}
+                if (File.Exists(_path + entry.Key + ".mp4") || File.Exists(_path + entry.Key + ".jpg")) continue;
                 if (entry.Value.Contains(".mp4"))
                 {
                     WebClient.DownloadFile(entry.Value, _path + entry.Key + ".mp4");
                 }
                 else
                 {
-                    WebClient.DownloadFile(entry.Value, _path + entry.Key + ".png");
+                    WebClient.DownloadFile(entry.Value, _path + entry.Key + ".jpg");
                 }
-               
-            }    
+            }
+            watch.Stop();
+            var downloadPicturesTime = watch.ElapsedMilliseconds;
+            Console.WriteLine("Time to download pictures: " + downloadPicturesTime/1000.00 + " seconds");
+            Console.WriteLine("Total Program Time: " + (enterPostTime + getPostPicturesTime +
+                                                        downloadPicturesTime)/1000.00 + " seconds");
             _driver.Quit();
         }
     }
