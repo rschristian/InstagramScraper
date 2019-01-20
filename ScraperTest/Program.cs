e2a8ff292005c8d730e1cfade2c8d57105a50658
Devildude4427 <ryanchristian4427@gmail.com>
2019-01-20 19:01:49 +0000

    Added wrapper project that tests the scraper's accuracy and time. Having issues with consistency. Chrome seems to now work better, though it too can get stuck.

---
+++ ScraperTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Instagram_Scraper;
+
+namespace ScraperTest
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var correctRunPercentage = 0;
+
+            var totalScraperRunTime = 0.000;
+            
+            const int testPasses = 5;
+            for (var i = 0; i < testPasses; i++)
+            {
+                using (var outputCapture = new OutputCapture())
+                {
+                    var watch = System.Diagnostics.Stopwatch.StartNew();
+                    
+                    WebScraper.SetUp("***REMOVED***", false, string.Empty, string.Empty,
+                        string.Empty, true, false);
+                
+                    var stuff = outputCapture.Captured.ToString();
+                    if (stuff.Contains("31 Downloading:")) correctRunPercentage++;
+                    
+                    watch.Stop();
+                    totalScraperRunTime = totalScraperRunTime + watch.ElapsedMilliseconds;
+                }
+            }
+            Console.WriteLine("{0}% of runs returned all links", correctRunPercentage*(100/testPasses));
+            
+            Console.WriteLine("It took the scraper {0} seconds on average, per test", totalScraperRunTime/(testPasses*1000.00));
+        }
+    }
+}
