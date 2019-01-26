d436ae057e1319a205426cf7c31e1b4cf812d8de
Devildude4427 <ryanchristian4427@gmail.com>
2019-01-26 18:30:26 +0000

    Formatting to DownloadManager.cs

--- Instagram_Scraper/Utility/DownloadManager.cs
+++ Instagram_Scraper/Utility/DownloadManager.cs
@@ -10,68 +10,62 @@ namespace Instagram_Scraper.Utility
     {
         public static async void ConsumeFilesAsync(string path, ISourceBlock<KeyValuePair<string, string>> source)
         {
-            if(!File.Exists(path)) Directory.CreateDirectory(path);
+            if (!File.Exists(path)) Directory.CreateDirectory(path);
             var filesProcessed = 0;
             var filesDownloaded = 0;
-            
+
             while (await source.OutputAvailableAsync())
             {
                 var client = new WebClient();
                 var (key, value) = source.Receive();
                 filesProcessed++;
-        
+
                 //Conflicted about whether or not this should exist. Doesn't save time, but can fix (or break)
                 //previous downloads.
-                var fileExists = Directory.GetFiles(path,key + ".*");
+                var fileExists = Directory.GetFiles(path, key + ".*");
                 if (fileExists.Length > 0) continue;
-                
-                Console.WriteLine((filesProcessed) + " Downloading: " + key);
-                
+
+                Console.WriteLine(filesProcessed + " Downloading: " + key);
+
                 if (value.Contains(".mp4"))
-                {
                     client.DownloadFileAsync(new Uri(value), path + key + ".mp4");
-                }
                 else
-                {
                     client.DownloadFileAsync(new Uri(value), path + key + ".jpg");
-                }
-                
+
                 filesDownloaded++;
             }
-            
+
             Console.WriteLine("Processed {0} files.", filesProcessed);
             Console.WriteLine("Downloaded {0} files.", filesDownloaded);
         }
-        
-        public static async void ConsumeTextAsync(string path, ISourceBlock<KeyValuePair<string, List<KeyValuePair<string, string>>>> source)
+
+        public static async void ConsumeTextAsync(string path,
+            ISourceBlock<KeyValuePair<string, List<KeyValuePair<string, string>>>> source)
         {
             var dirPath = path + "Text/";
-            if(!File.Exists(dirPath)) Directory.CreateDirectory(dirPath);
+            if (!File.Exists(dirPath)) Directory.CreateDirectory(dirPath);
             var textFilesProcessed = 0;
             var textFilesDownloaded = 0;
-            
+
             while (await source.OutputAvailableAsync())
             {
                 var (postDate, commentData) = source.Receive();
                 var filePath = dirPath + postDate + ".txt";
                 textFilesProcessed++;
-        
+
                 //TODO Comments may change, so there needs to be a way to update, potentially without removing old ones
                 if (File.Exists(filePath)) continue;
-                
-                Console.WriteLine((textFilesProcessed + 1) + " Downloading Text: " + postDate);
-                
-                using (var sw = File.CreateText(filePath)) 
+
+                Console.WriteLine(textFilesProcessed + " Downloading Text: " + postDate);
+
+                using (var sw = File.CreateText(filePath))
                 {
-                    foreach(var (user, commentText) in commentData)
-                    {
-                        sw.WriteLine(user + ": " + commentText);
-                    }
-                }	
-                
+                    foreach (var (user, commentText) in commentData) sw.WriteLine(user + ": " + commentText);
+                }
+
                 textFilesDownloaded++;
             }
-            
+
             Console.WriteLine("Processed {0} text files.", textFilesProcessed);
             Console.WriteLine("Downloaded {0} text files.", textFilesDownloaded);
         }
