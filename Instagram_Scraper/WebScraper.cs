78087ff3526eef1bedf15e86fa0db1880b6f2a5b
Devildude4427 <ryanchristian4427@gmail.com>
2019-02-03 00:04:39 +0000

    Combined chrome arguments

--- Instagram_Scraper/WebScraper.cs
+++ Instagram_Scraper/WebScraper.cs
@@ -30,9 +30,7 @@ namespace Instagram_Scraper
             {
                 var optionsChrome = new ChromeOptions();
                 optionsChrome.AddUserProfilePreference("profile.default_content_setting_values.images", 2);
-                optionsChrome.AddArgument("--disable-popup-blocking");
-                optionsChrome.AddArgument("--window-size=1920,1080");
-                optionsChrome.AddArgument("--mute-audio");
+                optionsChrome.AddArguments("--disable-popup-blocking", "--window-size=1920,1080", "--mute-audio");
 
                 if (scraperOptions.Headless) optionsChrome.AddArgument("headless");
                 _driver = new ChromeDriver(optionsChrome);
